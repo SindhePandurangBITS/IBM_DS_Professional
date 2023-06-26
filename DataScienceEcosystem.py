e24268d0d9bb619baf30eb028c0d1270a2aa9d44
SindhePandurangBITS <82663469+SindhePandurangBITS@users.noreply.github.com>
2023-06-26 14:32:53 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **objectives:**
+# - Python is a versatile programming language widely used in data science for its simplicity and extensive libraries.
+# - Data science techniques like data cleaning, exploratory data analysis, and machine learning algorithms are crucial for extracting insights from data.
+# - Python's powerful libraries such as NumPy, Pandas, and Matplotlib enable efficient data manipulation, analysis, and visualization.
+# - Understanding statistical concepts and hypothesis testing is fundamental for making informed decisions based on data.
+# - Data science projects involve the entire workflow, from data collection and preprocessing to model building, evaluation, and communication of results.
+
+# Some of the popular languages that Data Scientists use are
+# * Python 
+# * R
+# * Sql
+# * java
+
+# |  Data Science Tools |
+# |  ------------------ |
+# | R Studio            |
+# | Apache Spark        |
+# | TensorFlow          |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+#In this arithmetic expression we first multiply the integers and then we do addition.
+(3*4)+5
+
+
+# In[11]:
+
+
+#In this cell we will be converting minutes into hours by deviding with constant 60
+x = 200
+y = x/60
+print(y)
+
+
+# Author
+# ## SINDHE PANDURANG
